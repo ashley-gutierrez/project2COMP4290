77d52a65dc106c1bcb2d6cd470d6a01fbaff9783
ashley <gutierrezgallegos2@otterbein.edu>
2025-10-13 21:01:48 -0400

    added decryption with inverse methods. Stopped returning newState and setting state=newState and made all methods private static

--- src/Chat.java
+++ src/Chat.java
@@ -23,7 +23,7 @@ public class Chat {
     public static final BigInteger G = new BigInteger("105003596169089394773278740673883282922302458450353634151991199816363405534040161825176553806702944696699090103171939463118920452576175890312021100994471453870037718208222180811650804379510819329594775775023182511986555583053247825364627124790486621568154018452705388790732042842238310957220975500918398046266");
     public static final int LENGTH = 1023;
     private byte[] key;
-    int[] sBox = {0x63,0x7c,0x77,0x7b,0xf2,0x6b,0x6f,0xc5,0x30,0x01,0x67,0x2b,0xfe,0xd7,0xab,0x76,
+    public static int[] sBox = {0x63,0x7c,0x77,0x7b,0xf2,0x6b,0x6f,0xc5,0x30,0x01,0x67,0x2b,0xfe,0xd7,0xab,0x76,
             0xca,0x82,0xc9,0x7d,0xfa,0x59,0x47, 0xf0,0xad,0xd4,0xa2,0xaf,0x9c,0xa4,0x72,0xc0,
             0xb7,0xfd,0x93,0x26,0x36,0x3f,0xf7,0xcc,0x34,0xa5,0xe5,0xf1,0x71,0xd8,0x31,0x15,
             0x04,0xc7,0x23,0xc3,0x18,0x96,0x05,0x9a,0x07,0x12,0x80,0xe2,0xeb,0x27,0xb2,0x75,
@@ -41,7 +41,24 @@ public class Chat {
             0xe9,0xce,0x55,0x28,0xdf,
             0x8c,0xa1,0x89,0x0d,0xbf,0xe6,0x42,0x68,0x41,0x99,0x2d,0x0f,0xb0,0x54,0xbb,0x16};
 
-    int[] rcon = {0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a,
+    public static int[] inverseBox = {0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB,
+            0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB,
+            0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,
+            0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25,
+            0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,
+            0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84,
+            0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06,
+            0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B,
+            0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73,
+            0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E,
+            0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B,
+            0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,
+            0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F,
+            0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF,
+            0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61,
+            0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D};
+
+    public static int[] rcon = {0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a,
             0x2f, 0x5e, 0xbc, 0x63, 0xc6, 0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91, 0x39,
             0x72, 0xe4, 0xd3, 0xbd, 0x61, 0xc2, 0x9f, 0x25, 0x4a, 0x94, 0x33, 0x66, 0xcc, 0x83, 0x1d, 0x3a,
             0x74, 0xe8, 0xcb, 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8,
@@ -189,12 +206,16 @@ public class Chat {
                                 0x4F, 0x6E, 0x65, 0x20,
                                 0x4E, 0x69, 0x6E, 0x65,
                                 0x20, 0x54, 0x77, 0x6F};
-
+                        System.out.println("The plaintext encrypted is:");
                         byte[][] ciphertext = encrypt(key, plaintext);
                         for (int i = 0; i < ciphertext.length; ++i) {
                             System.out.println(toHex(ciphertext[i]));
                         }
-
+                        System.out.println("The ciphertext decrypted is:");
+                        byte[][] decryptedText = decrypt(key, getByteArray(ciphertext));
+                        for (int i = 0; i < decryptedText.length; ++i) {
+                            System.out.println(toHex(decryptedText[i]));
+                        }
 
 
                         netOut.writeObject(bytes);
@@ -211,21 +232,35 @@ public class Chat {
             }
         }
     }
-    private byte[] xorByteArrays (byte[] array1, byte[] array2) {
+
+    private static byte[] xorByteArrays (byte[] array1, byte[] array2) {
         byte[] result = new byte[array1.length];
         for (int i = 0; i < array1.length; ++i) {
             result[i] = (byte) (array1[i] ^ array2[i]);
         }
         return result;
     }
-    private String toHex(byte[] array) {
+
+    private static String toHex(byte[] array) {
         StringBuilder hex = new StringBuilder();
         for (int i = 0; i < array.length; ++i) {
             hex.append(String.format("%02X ", array[i] & 0xFF));
         }
         return hex.toString();
     }
-    private byte[] getRoundKey(byte[] previous, int round) {
+
+    private static byte[] getByteArray(byte[][] state) {
+        byte[] array = new byte[state.length * state.length];
+        int index = 0;
+        for (int column = 0; column < state.length; ++column) {
+            for (int row = 0; row < state.length; ++row) {
+                array[index++] = state[row][column];
+            }
+        }
+        return array;
+    }
+
+    private static byte[] getRoundKey(byte[] previous, int round) {
         //splits key
         byte [] zero = Arrays.copyOfRange(previous, 0, 4);
         byte [] one = Arrays.copyOfRange(previous, 4, 8);
@@ -263,54 +298,48 @@ public class Chat {
         //System.out.println("Roundkey: " + toHex(roundKey));
         return roundKey;
     }
-    private byte[][] addRoundKey(byte[][] state, byte[] roundKey) {
-        byte[][] newState = new byte[4][4];
+
+    private static void addRoundKey(byte[][] state, byte[] roundKey) {
         int index = 0;
         for(int column = 0; column < state.length; ++column) {
             for (int row = 0; row < state.length; ++row) {
-                newState[row][column] = (byte) (state[row][column] ^ roundKey[index++]);
+                state[row][column] ^= roundKey[index++];
             }
         }
-        return newState;
     }
 
-    private byte[][] substituteBytes(byte[][] state) {
-        byte[][] newState = new byte[4][4];
-        for(int column = 0; column < state.length; ++column) {
-            for (int row = 0; row < state.length; ++row) {
-                newState[row][column] = (byte) sBox[state[row][column] & 0xFF];
+    private static void substituteBytes(byte[][] state) {
+        for(int row = 0; row < state.length; ++row) {
+            for (int column = 0; column < state.length; ++column) {
+                state[row][column] = (byte) sBox[state[row][column] & 0xFF];
             }
         }
-        return newState;
     }
 
-    private byte[][] shiftRows(byte[][] state) {
-        byte[][] newState = new byte[4][4];
-        for (int row = 0; row < state.length; ++row) {
+    private static void shiftRows(byte[][] state) {
+        for (int row = 1; row < state.length; ++row) {
+            byte[] temp = new byte[state.length];
             for(int column = 0; column < state.length; ++column) {
-                newState[row][column] = state[row][(column + row) % state.length];
+                temp[column] = state[row][(column + row) % state.length];
             }
+            System.arraycopy(temp, 0, state[row], 0, state.length);
         }
-        return newState;
     }
 
-    private static byte[][] mixColumns(byte[][] state) {
-        byte[][] newState = new byte[4][4];
+    private static void mixColumns(byte[][] state) {
         byte[] a = new byte[4];
         for (int j = 0; j < state.length; ++j) {
             for (int i = 0; i < state.length; ++i)
                 a[i] = state[i][j];
 
-            newState[0][j] = (byte) (galoisMultiply(a[0],2) ^ galoisMultiply(a[1],3) ^ a[2] ^ a[3]);
-            newState[1][j] = (byte) (a[0] ^ galoisMultiply(a[1],2) ^ galoisMultiply(a[2],3) ^ a[3]);
-            newState[2][j] = (byte) (a[0] ^ a[1] ^ galoisMultiply(a[2],2) ^ galoisMultiply(a[3],3));
-            newState[3][j] = (byte) (galoisMultiply(a[0],3) ^ a[1] ^ a[2] ^ galoisMultiply(a[3],2));
+            state[0][j] = (byte) (galoisMultiply(a[0],2) ^ galoisMultiply(a[1],3) ^ a[2] ^ a[3]);
+            state[1][j] = (byte) (a[0] ^ galoisMultiply(a[1],2) ^ galoisMultiply(a[2],3) ^ a[3]);
+            state[2][j] = (byte) (a[0] ^ a[1] ^ galoisMultiply(a[2],2) ^ galoisMultiply(a[3],3));
+            state[3][j] = (byte) (galoisMultiply(a[0],3) ^ a[1] ^ a[2] ^ galoisMultiply(a[3],2));
         }
-        return newState;
     }
 
-    private static byte[][] inverseMixColumns(byte[][] state) {
-        byte[][] newState = new byte[4][4];
+    private static void inverseMixColumns(byte[][] state) {
         byte[] a = new byte[4];
         for (int j = 0; j < state.length; ++j) {
             for (int i = 0; i < state.length; ++i)
@@ -321,7 +350,6 @@ public class Chat {
             state[2][j] = (byte) (galoisMultiply(a[2],14) ^ galoisMultiply(a[1],9) ^ galoisMultiply(a[0],13) ^ galoisMultiply(a[3],11));
             state[3][j] = (byte) (galoisMultiply(a[3],14) ^ galoisMultiply(a[2],9) ^ galoisMultiply(a[1],13) ^ galoisMultiply(a[0],11));
         }
-        return newState;
     }
 
     private static byte galoisMultiply(int a, int b) {
@@ -342,7 +370,7 @@ public class Chat {
 
         return (byte)p;
     }
-    private byte[][] loadState(byte[] plaintext) {
+    private static byte[][] loadState(byte[] plaintext) {
         byte[][] state = new byte[4][4];
         int index = 0;
         for(int column = 0; column < state.length; ++column) {
@@ -353,7 +381,7 @@ public class Chat {
         return state;
     }
 
-    private byte[][] encrypt(byte[] key, byte[] plaintext) {
+    private static byte[][] encrypt(byte[] key, byte[] plaintext) {
         byte[][] state = loadState(plaintext);
         byte[][] roundKeys = new byte[11][16];
         roundKeys[0] = key;
@@ -361,19 +389,62 @@ public class Chat {
             roundKeys[i] = getRoundKey(roundKeys[i - 1], i);
         }
         //initial round
-        state = addRoundKey(state, roundKeys[0]);
+        addRoundKey(state, roundKeys[0]);
         //normal rounds 1-9
         for (int i = 1; i <=9; ++i){
-            state = substituteBytes(state);
-            state = shiftRows(state);
-            state = mixColumns(state);
-            state = addRoundKey(state, roundKeys[i]);
+            substituteBytes(state);
+            shiftRows(state);
+            mixColumns(state);
+            addRoundKey(state, roundKeys[i]);
         }
         //final round
-        state = substituteBytes(state);
-        state = shiftRows(state);
-        state = addRoundKey(state, roundKeys[10]);
+        substituteBytes(state);
+        shiftRows(state);
+        addRoundKey(state, roundKeys[10]);
         return state;
+    }
+
+    private static byte[][] decrypt(byte[] key, byte[] ciphertext) {
+        byte[][] state = loadState(ciphertext);
+        byte[][] roundKeys = new byte[11][16];
+        roundKeys[0] = key;
+        for (int i = 1; i <= roundKeys.length - 1; ++i) {
+            roundKeys[i] = getRoundKey(roundKeys[i - 1], i);
+        }
+        //inverse final round
+        addRoundKey(state, roundKeys[10]);
+        inverseShiftRows(state);
+        inverseSubstituteBytes(state);
+
+        //inverse normal rounds 9-1
+        for (int i = 9; i >= 1; --i){
+            addRoundKey(state, roundKeys[i]);
+            inverseMixColumns(state);
+            inverseShiftRows(state);
+            inverseSubstituteBytes(state);
+        }
+        //inverse initial round
+        addRoundKey(state, roundKeys[0]);
+
+        return state;
+    }
+
+    private static void inverseShiftRows(byte[][] state) {
+        for (int row = 0; row < state.length; ++row) {
+            byte[] temp = new byte[state.length];
+            for(int column = 0; column < state.length; ++column) {
+                temp[column] = state[row][(column - row + state.length) % state.length];
+            }
+            System.arraycopy(temp, 0, state[row], 0, state.length);
+        }
+    }
+
+    private static void inverseSubstituteBytes(byte[][] state) {
+        for (int row = 0; row < state.length; ++row) {
+                for(int column = 0; column < state.length; ++column) {
+                    state[row][column] = (byte) inverseBox[state[row][column] & 0xFF];
+            }
+        }
     }
 
     private class Receiver extends Thread {
